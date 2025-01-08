c809836dc927fb9d3a67d7209057766a7b556b07
Joana Silva <101148531+JoanaSilvaHUS@users.noreply.github.com>
2025-01-08 15:10:24 +0000

    ASP.NET CORE MVC Initial template

---
+++ PV_VitalEase/PV_VitalEase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PV_VitalEase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
