32318c3a240ce981285ac6388902cf17ad5c8a6c
Rita-Briosa <117911671+Rita-Briosa@users.noreply.github.com>
2025-01-16 23:04:01 +0000

    Forgot Pass & Reset Pass

--- PV_VitalEase/vitalease.client/src/app/app.module.ts
+++ PV_VitalEase/vitalease.client/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { homePageComponent } from './homePage/homePage.component';
 import { LoginComponent } from './login/login.component';
+import { ResetPassComponent } from './reset-pass/reset-pass.component';
+import { ForgotPassComponent } from './forgot-pass/forgot-pass.component';
 
 @NgModule({
   declarations: [
-    AppComponent, homePageComponent,LoginComponent
+    AppComponent, homePageComponent,LoginComponent, ResetPassComponent, ForgotPassComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
---
+++ PV_VitalEase/vitalease.client/src/app/forgot-pass/forgot-pass.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-pass',
+  standalone: false,
+  
+  templateUrl: './forgot-pass.component.html',
+  styleUrl: './forgot-pass.component.css'
+})
+export class ForgotPassComponent {
+
+}
