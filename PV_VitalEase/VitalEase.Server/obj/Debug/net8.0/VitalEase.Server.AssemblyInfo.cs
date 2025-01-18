d4c7d5d61a6708eb925202cafa3daff232be71d3
José Lúcio <202100198@estudantes.ips.pt>
2025-01-18 03:27:26 +0000

    Email vital ease

--- PV_VitalEase/VitalEase.Server/obj/Debug/net8.0/VitalEase.Server.AssemblyInfo.cs
+++ PV_VitalEase/VitalEase.Server/obj/Debug/net8.0/VitalEase.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VitalEase.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c4c67c1e6d3805da4e4942aa4f345302f647537")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7bea3c1acda9fc2a5b69cbceca4d16e62fb7099")]
 [assembly: System.Reflection.AssemblyProductAttribute("VitalEase.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VitalEase.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
