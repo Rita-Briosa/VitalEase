a12ead55b7627761bf65eb2db0a906d0be1b3439
andrelopes772 <202002285@estudantes.ips.pt>
2025-01-18 17:51:34 +0000

    AboutUs

--- PV_VitalEase/vitalease.client/src/app/about-us/about-us.component.ts
+++ PV_VitalEase/vitalease.client/src/app/about-us/about-us.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
@@ -8,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
+  userInfo: any = null;
+  isLoggedIn: boolean = false;
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+    // Check if user is logged in by fetching the user info
+    this.isLoggedIn = this.authService.isLoggedIn();  // A verificação da sessão agora é feita com o método isLoggedIn()
+    if (this.isLoggedIn) {
+      this.userInfo = this.authService.getUserInfo();  // Se estiver logado, pega as informações do usuário
+    }
+  }
+
+  logout() {
+    this.authService.logout();
+    this.isLoggedIn = false;
+    this.router.navigate(['/']);
+  }
 }
