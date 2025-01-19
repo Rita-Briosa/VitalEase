6f199e91f031fa0dcb6e9cd318c17e8606a97a9c
José Lúcio <202100198@estudantes.ips.pt>
2025-01-19 06:50:55 +0000

    Correção acesso à página resetPassword

--- PV_VitalEase/VitalEase.Server/obj/Debug/net8.0/VitalEase.Server.AssemblyInfo.cs
+++ PV_VitalEase/VitalEase.Server/obj/Debug/net8.0/VitalEase.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VitalEase.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3affff7839d18bc763d2e8fc692541d564ce8dea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94235774800ecc23960e9d9eea378525e9f1b66c")]
 [assembly: System.Reflection.AssemblyProductAttribute("VitalEase.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VitalEase.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
